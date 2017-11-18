3654d47e2dda0ae85f199b56de68a77b5f2b37b6
Madel Asistio <madel.asistio@gmail.com>
2017-11-18 00:12:22 -0800

    doen

---
+++ tumblrFeedV2/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  tumblrFeedV2
+//
+//  Created by Madel Asistio on 11/11/17.
+//  Copyright © 2017 Madel Asistio. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tumblrFeedV2/tumblr.swift
@@ -0,0 +1,9 @@
+//
+//  tumblr.swift
+//  tumblrFeedV2
+//
+//  Created by Madel Asistio on 11/11/17.
+//  Copyright © 2017 Madel Asistio. All rights reserved.
+//
+
+import Foundation
